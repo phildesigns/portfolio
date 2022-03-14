dfdeeee9ca7849afa1363b01141ea4470ad969c6
Phil <philemon.varret@gmail.com>
2022-03-14 12:26:27 +0100

    Add Header and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+  <footer>
+    <div class="footer-section">
+      <h3>Plan du site</h3>
+      <ul>
+        <li><a>Accueil <b>></b></a></li>
+        <li><a>Projets <b>></b></a></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h3>Informations</h3>
+      <ul>
+        <li><a>Contact <b>></b></a></li>
+        <li><a>Mentions légales <b>></b></a></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h3>Plus</h3>
+      <ul class="socials">
+        <li><a href="https://github.com/miowski"><i class="fi-snsuxl-github-alt"></i></a></li>
+        <li><a href="https://linkedin.com/in/philemonvarret"><i class="fi-snsuxl-linkedin"></i></a></li>
+      </ul>
+    </div>
+  </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+footer {
+  margin-bottom: 2em;
+}
+
+.footer-section {
+  border-top: solid 3px var(--contrast);
+}
+
+footer {
+  text-align: center;
+}
+
+footer h3 {
+  font-size: 1.5rem;
+}
+
+footer a {
+  font-size: 1rem;
+}
+
+@media only screen and (min-width: 768px) {
+  footer {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .footer-section {
+    border-top: none;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <h1>Phil V.</h1>
+        <div class="nav-toggle" @click="openMenu()">
+          <div class="nav-toggle-bar">
+          </div>
+        </div>
+        <div class="menu" id="menu">
+          <div class="menu-content">
+            <ul>
+              <li><a>projets <b>></b></a></li>
+              <li><a>contact <b>></b></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  methods: {
+    openMenu: function () {
+      document.documentElement.classList.toggle("menu-open");
+    }
+  }
+}
+</script>
+
+<style>
+nav li {
+  font-size: 1.125rem;
+  margin-left: 1.5rem;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  height: 5em;
+}
+
+.menu {
+  z-index: 1;
+  position: fixed;
+  top: 5rem;
+  transform: translateX(100vw);
+  transition: all .4s;
+  background: #202020;
+  height: 100vh;
+  width: calc(100vw - 1.5rem)
+}
+
+.menu-content {
+  border-top: 3px solid #202020;
+}
+
+.menu-content li {
+  position: relative;
+  margin-top: 1.5rem;
+  right: 3rem;
+  text-align: right;
+  width: 100%;
+}
+
+.menu-open .menu {
+  transform: translateX(0);
+}
+
+.menu-open, .menu-open body {
+  overflow: hidden;
+}
+
+.nav-toggle {
+  position: absolute;
+  right: 1em;
+  top: 1em;
+  width: 2.55rem;
+  height: 2.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.nav-toggle-bar,
+.nav-toggle-bar::before,
+.nav-toggle-bar::after {
+  content: "";
+  background-color: #F2F2F2;
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 3px;
+  transition: all .4s ease;
+}
+
+.nav-toggle-bar::before {
+  top: -1rem;
+}
+
+.nav-toggle-bar::after {
+  top: 1rem;
+}
+
+.menu-open .nav-toggle-bar {
+  transform: rotate(135deg);
+}
+
+.menu-open .nav-toggle-bar::before,
+.menu-open .nav-toggle-bar::after {
+  transform: rotate(90deg);
+  top: 0;
+}
+</style>
