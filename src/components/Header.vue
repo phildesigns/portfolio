2fbd04c555fbc9711a7940bc52f1c2abd99ec09f
Phil <philemon.varret@gmail.com>
2022-03-14 15:22:18 +0100

    Update Header
    Simplify by removing / replacing items.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,21 +2,14 @@
   <div>
     <header>
       <nav>
+        <h1>phil</h1>
         <router-link to="/">
-          <h1>Phil V.</h1>
+          <svg id="monogram" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 1024">
+          <path d="m453 492-14-4-338-195 53-32 299 172 295-169 58 29-339 195-14 4zM142 753l-55-33V323l257 140c72 42 106 121 105 176 0 36-13 64-37 77-18 11-49 17-96-10L142 605v148Zm0-211 202 117c21 12 35 13 41 10s10-15 10-30c0-41-27-98-79-128L142 410v132ZM817 475c1-38-12-68-37-83-17-11-48-18-93 6L475 514l-14 24v391l55-33V750l140-77 97 91 49-29-95-90 6-3c70-39 103-112 104-167Zm-301 79 197-108c17-9 31-12 39-7 7 4 11 18 11 35-1 38-25 92-76 120l-171 94V554Z"/>
+          <path id="outline" d="M463 1024h-27L13 780 0 756V268l13-24L436 0h27l423 244 13 24v488l-13 24-423 244ZM54 740l396 229 395-229V284L450 55 54 284v456Z"/>
+        </svg>
         </router-link>
-        <div class="nav-toggle" @click="openMenu()">
-          <div class="nav-toggle-bar">
-          </div>
-        </div>
-        <div class="menu" id="menu">
-          <div class="menu-content">
-            <ul>
-              <li><a>projets <b>></b></a></li>
-              <li><a>contact <b>></b></a></li>
-            </ul>
-          </div>
-        </div>
+        <h1>varret</h1>
       </nav>
     </header>
   </div>
@@ -35,105 +28,38 @@ export default {
 </script>
 
 <style>
-nav li {
-  font-size: 1.125rem;
-  margin-left: 1.5rem;
-}
+
 
 nav {
   display: flex;
-  justify-content: space-between;
-  height: 5em;
-}
-
-.menu {
-  z-index: 1;
-  position: fixed;
-  top: 5rem;
-  transform: translateX(100vw);
-  transition: all .4s;
-  background: #202020;
-  height: 100vh;
-  width: calc(100vw - 1.5rem)
-}
-
-.menu-content {
-  border-top: 3px solid #202020;
-}
-
-.menu-content li {
-  position: relative;
-  margin-top: 1.5rem;
-  right: 3rem;
-  text-align: right;
-  width: 100%;
-}
-
-.menu-open .menu {
-  transform: translateX(0);
-}
-
-.menu-open, .menu-open body {
-  overflow: hidden;
-}
-
-.nav-toggle {
-  position: absolute;
-  right: 1em;
-  top: 1em;
-  width: 2.55rem;
-  height: 2.25rem;
-  display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 5em;
+  margin: 1em;
 }
 
-.nav-toggle-bar,
-.nav-toggle-bar::before,
-.nav-toggle-bar::after {
-  content: "";
-  background-color: #F2F2F2;
-  position: absolute;
-  left: 0;
-  width: 100%;
-  height: 3px;
-  transition: all .4s ease;
+nav h1 {
+   color: var(--contrast);
+ }
+
+#monogram {
+  overflow: clip;
+  height: 5em;
+  margin: auto 1em;
 }
 
-.nav-toggle-bar::before {
-  top: -1rem;
+#monogram * {
+  fill: var(--contrast);
 }
 
-.nav-toggle-bar::after {
-  top: 1rem;
+#monogram #outline {
+  transition: ease-in-out .10s;
+  transform-origin: center;
 }
 
-.menu-open .nav-toggle-bar {
-  transform: rotate(135deg);
+#monogram:hover #outline {
+  transform: scale(110%);
 }
 
-.menu-open .nav-toggle-bar::before,
-.menu-open .nav-toggle-bar::after {
-  transform: rotate(90deg);
-  top: 0;
-}
-
-@media only screen and (min-width: 768px) {
-  .menu {
-    transform: translateX(0);
-    height: auto;
-    position: relative;
-    top: 0;
-    width: auto;
-  }
-
-  .menu-content li {
-    display: inline;
-    right: 0;
-  }
-
-  .nav-toggle {
-    display: none;
-  }
-}
 </style>
