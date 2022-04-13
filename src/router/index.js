f273b8eb30c17df453ae2dc813bc81dec1bedda3
Phil <philemon.varret@gmail.com>
2022-04-13 14:56:39 +0200

    Add title

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,10 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: "Phil's Portfolio"
+    }
   },
   { path: '/project/:id(\\d+)',
     name: 'project',
